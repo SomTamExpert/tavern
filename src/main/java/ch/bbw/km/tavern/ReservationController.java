97481e514951e0504009476f54df0473b5ed43ab
Marco Karpf <2marco.karpf2@gmail.com>
2022-03-15 21:43:57 +0100

    refactoring reservation controller

--- src/main/java/ch/bbw/km/tavern/ReservationController.java
+++ src/main/java/ch/bbw/km/tavern/ReservationController.java
@@ -15,24 +15,23 @@ public class ReservationController {
 //      return "redirect:/reservationlistview.html";
 //   }
 
-    @GetMapping("/addesreservation")
-    public String addReservation(HttpServletRequest request){
+    @GetMapping("/addreservation")
+    public String addReservation(HttpServletRequest request) {
         System.out.println("ReservationController.addReservation");
         printRequest(request);
         return "reservationlistview";
     }
 
-    public void printRequest(HttpServletRequest request) {
+    private void printRequest(HttpServletRequest request) {
         System.out.println("Header:");
         Collections.list(request.getHeaderNames()).forEach((name) -> {
             System.out.println("\t" + name + ": " + request.getHeader(name));
         });
-        System.out.println("Querry String:");
+        System.out.println("QueryString:");
         System.out.println("\t" + request.getQueryString());
-
         System.out.println("Body Parameters:");
         request.getParameterMap().forEach((name, value) -> {
-            System.out.println("\t" + name + ":" + request.getParameter(name));
+            System.out.println("\t" + name + ": " + request.getParameter(name));
         });
     }
 }
